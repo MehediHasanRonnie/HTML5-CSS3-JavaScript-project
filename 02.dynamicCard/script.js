8014e437563773b81d18e754e693d0c812dfabc5
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-30 13:41:07 +0600

    commited

---
+++ 02.dynamicCard/script.js
@@ -0,0 +1,50 @@
+//dummy data
+const posts = [
+  {
+    title: "this is title 1",
+    body: "this is body 1",
+  },
+  {
+    title: "this is title 2",
+    body: "this is body 2",
+  },
+  {
+    title: "this is title 3",
+    body: "this is body 3",
+  },
+  {
+    title: "this is title 4",
+    body: "this is body 4",
+  },
+];
+
+/* <div class="post">
+<h4 class="post-title">post title 1</h4>
+<p class="post-body">post description1</p>
+</div> */
+
+//fetch data fro api
+// const fecthData = async (config) => {
+//   try {
+//     const res = await axios(config);
+//     return res.data;
+//   } catch (error) {
+//     throw Error("data is not fetched");
+//   }
+// };
+// //selection
+
+const postsElement = document.querySelector(".posts");
+
+const loadAllData = async () => {
+  posts.map((post) => {
+    const postElements = document.createElement("div");
+    postElements.classList.add("post");
+    postElements.innerHTML = `
+    <h4 class="post-title">${post.title}</h4>
+    <p class="post-body">${post.body}</p>
+    `;
+    postsElement.appendChild(postElements);
+  });
+};
+loadAllData();
